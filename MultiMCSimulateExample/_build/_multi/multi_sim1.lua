2b0b69d514cf2dc7c8f046c7be323d02f73941a9
Gordon <>
2021-12-15 17:10:27 +0000

    Example Multi-simulator support

---
+++ MultiMCSimulateExample/_build/_multi/multi_sim1.lua
@@ -0,0 +1,46 @@
+require("_build._multi.simulate_multiple")
+
+
+-----------LOAD MCS-------------------------------------------------------------------------------
+-- set your MCs here
+-- LoadMC takes the same parameter as require(...)
+-- Order matters, they will draw to screen in this order (last over the top)
+
+local loadingScreen = LoadMC("Loading_Screen")
+local navigation    = LoadMC("Navigation_at_top_of_page")
+local menuLayout    = LoadMC("Menu_Layout")
+
+
+-----------CONFIG----------------------------------------------------------------------------------
+-- configure how many screens to use
+__simulator.config:configureScreen(1, "2x2", true, false)
+__simulator.config:configureScreen(2, "2x2", true, false)
+
+-- connect the MCs to each other
+
+-- set "loaded" input after 3 seconds
+loadingScreen.__simulator.config:addBoolHandler(10, function() return loadingScreen._globalTicks > 180 end)
+loadingScreen.__simulator.config:addBoolHandler(11, function() return loadingScreen._globalTicks > 360 end)
+
+
+-- wait for loading screen to set output 11
+--  and only connect to Monitor 1
+navigation.__simulator.config:addNumberHandler(2,
+function(a) return loadingScreen.output._bools[11] and 1 or 0 end) -- once loadingScreen is done
+navigation.onLBSimulatorShouldDraw = function (screenNumber) return screenNumber == 1 end
+
+
+-- wait for loading screen to set output 11
+--  and only connect to Monitor 1
+menuLayout.__simulator.config:addNumberHandler(29,
+function(a) return loadingScreen.output._bools[11] and 1 or 0 end
+)
+menuLayout.onLBSimulatorShouldDraw = function (screenNumber) return screenNumber == 1 end
+
+
+
+
+-----------CONFIG----------------------------------------------------------------------------------
+-- do not remove this
+onTick = multiTick
+onDraw = multiDraw
